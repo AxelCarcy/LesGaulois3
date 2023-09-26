7e199f14ffabd41f175c7f44cfd4099a6577eb73
AxelCarcy <carcyaxel@gmail.com>
2023-09-26 20:03:36 +0200

    Les énumérés
    Modifier la méthode sEquiper

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,13 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement [] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -35,11 +38,67 @@ public class Romain {
 		assert force < forceAssert;
 	}
 	
+	public void sEquiper(Equipement equip) {
+		switch (nbEquipement) {
+		
+		case 0:
+			if (equip == Equipement.CASQUE) {
+				equipements[0] = Equipement.CASQUE;
+				System.out.println("Le soldat " + nom + " s’équipe avec un casque.");
+			} else {
+				equipements[1] = Equipement.BOUCLIER;
+				System.out.println("Le soldat " + nom + " s'équipe avec un bouclier.");
+
+			}
+			nbEquipement += 1;
+			break;
+			
+		case 1:
+			if (equip == Equipement.CASQUE) {
+				if (equipements[0] == Equipement.CASQUE) {
+					System.out.println("Le soldat " + nom + " possède déjà un casque !");
+				}
+				else {
+					equipements[0] = Equipement.CASQUE;
+					System.out.println("Le soldat " + nom + " s'équipe d'un casque. ");
+					nbEquipement += 1;
+				}
+			}
+			
+			else if (equip == Equipement.BOUCLIER) {
+				if (equipements[1] == Equipement.BOUCLIER) {
+					System.out.println("Le soldat " + nom + " possède déjà un bouclier !");
+				}
+				else {
+					equipements[1] = Equipement.BOUCLIER;
+					System.out.println("Le soldat " + nom + " s'équipe avec un bouclier. ");
+					nbEquipement += 1;
+				}
+			}
+			break;
+			
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+
+		default:
+			System.out.println("Erreur nbEquipement");
+			break;
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
 		
 		minus.prendreParole();
 		minus.parler("Bonjour");
-		minus.recevoirCoup(8);
+//		minus.recevoirCoup(8);
+		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
+		
+		
 	}
 }
